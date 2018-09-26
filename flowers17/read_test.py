16d63e481fbd80fbabe1202fd517ab9f4b2a176c
HDB <huangdunbo@126.com>
2018-09-26 15:54:31 +0800

    Add flower17 scripts.

--- flowers17/read_test.py
+++ flowers17/read_test.py
@@ -10,8 +10,14 @@ import flowers17_448
 images, onehot_labels, labels = flowers17_448.inputs("./flower17_448Train.tfrecord", "./flower17_448Val.tfrecord", "Train", 10, None)
 
 with tf.Session() as sess:
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess,coord=coord)
+
     imgs, clas = sess.run([images, labels])
 
     for i, img in enumerate(imgs):
         cv2.imwrite(str(i)+".jpg", img)
         print str(clas[i])
+
+    coord.request_stop()
+    coord.join(threads)
